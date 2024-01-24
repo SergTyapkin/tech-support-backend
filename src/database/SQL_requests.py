00b6361586280c3a8aba983cab997e2681afed63
Сергей Тяпкин <Tyapkin2002@mail.ru>
2024-01-24 06:16:20 +0300

    Захардкодил выписку по мероприятиям

--- src/database/SQL_requests.py
+++ src/database/SQL_requests.py
@@ -84,7 +84,8 @@ selectParticipationsExtract = \
     "JOIN events ON participations.eventid = events.id " \
     "JOIN positions ON participations.positionid = positions.id " \
     "WHERE userid = %s "\
-    "AND events.date BETWEEN (SELECT dateStart FROM periods WHERE NOW() BETWEEN dateStart AND dateEnd) AND (SELECT dateStart FROM periods WHERE NOW() BETWEEN dateStart AND dateEnd)"
+    "AND events.date BETWEEN 2023-09-01 AND 2023-12-31"
+    # "AND events.date BETWEEN (SELECT dateStart FROM periods WHERE NOW() BETWEEN dateStart AND dateEnd) AND (SELECT dateStart FROM periods WHERE NOW() BETWEEN dateStart AND dateEnd)"
 
 # ----- UPDATES -----
 updateUserById = \
